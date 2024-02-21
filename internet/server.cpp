80185aae7bfc35a529a2c00245734e594c490ad5
Raven <1772133019@qq.com>
2024-02-21 16:04:28 +0800

    小优化：使代码更模块化

--- internet/server.cpp
+++ internet/server.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 extern socklen_t serverAddrLength;
 extern socklen_t clientAddrLength;
+int connectfd = -2;
 
 int main(){
     tool::clear();
@@ -13,59 +14,24 @@ int main(){
 //2. 绑定IP地址和端口号
     // 定义成员
     initializeServerAddr(serverAddr, serverAddrLength);
-    int bindReturn = bind(sockfd, (struct sockaddr *)&serverAddr, static_cast<socklen_t>(serverAddrLength));
-    if(errorIfValueLessThan_0(bindReturn, "Failed to bind") != -1)
-    cout<<"- Bind successful -\n"<<"INET is "<<inet_ntoa(serverAddr.sin_addr)<<" / "<<SERV_PORT<<endl<<endl;; // 不能输出自动识别的ip
-
+    bindSocketToServer(sockfd);
+    
 //3. 设置监听套接字
-    int listenReturn = listen(sockfd, 5);
-    if(errorIfValueLessThan_0(listenReturn, "Failed to listen") != -1)
-    cout<<"- Listen successful -\n";
+    startListeningOnSocket(sockfd);
 
 //4. 接受客户端链接
     initializeClientAddr(clientAddr, clientAddrLength);
     
-    
-
 //4.5 accept
 
-    int connectfd = -2;
 
     while(1){ // 这个循环使得程序可以连接第二个客户端
-        cout<<"waiting for a new client...\n\n";
-        connectfd = accept(sockfd, (struct sockaddr *)&clientAddr, &clientAddrLength);
-        if(errorIfValueLessThan_0(connectfd, "Failed to connect and create a new connectfd") != -1)
-        cout<<"- Client connected -\n";
-        cout<<"Client IP is "<<inet_ntoa(clientAddr.sin_addr)<<" / "<<SERV_PORT<<endl<<endl;; // 不能输出自动识别的ip
-        
+        printMessage("waiting for a new client...\n");
+        acceptConnect(connectfd, sockfd);
+
 //5. 数据接受和发送 read
-        char buffer[64];
-        while(1){
-            memset(buffer, 0, sizeof(buffer));
-            int readReturn = read(connectfd, buffer, 64);
-
-            if(readReturn < 0){
-                perror("Failed to read");
-                cerr << "Error code: " << errno << endl;
-                return -1;
-            }else if(readReturn == 0){
-                cout << "\n-- The client disconnected! --\n";
-                cout<<"Client IP is "<<inet_ntoa(clientAddr.sin_addr)<<" / "<<SERV_PORT<<endl<<endl; // 不能输出自动识别的ip
-                break;
-            }
-            cout << "- read successful -\n"<<"recieved message ("<< readReturn <<" bytes):\n"<<buffer<<"\n\n";
-            
-
-            // if(errorIfValueLessThan_0(readReturn, "Failed to read buffer") != -1)
-            // cout << "- read successful -\n"<<"recieved message ("<< readReturn <<" bytes):\n"<<buffer<<"\n\n";
-            // else if(readReturn == 0){
-            //     cout << "\n-- The client disconnected! --\n";
-            //     cout<<"Client IP is "<<inet_ntoa(clientAddr.sin_addr)<<" / "<<SERV_PORT<<endl<<endl; // 不能输出自动识别的ip
-            //     break;
-            // }
-        }
-
-
+        readClientMessage(connectfd);
+        
         closeSocket(connectfd);
     } // 循环在4.5处开始
     closeSocket(sockfd);
--- internet/tcp.h
+++ internet/tcp.h
@@ -31,6 +31,7 @@
 #define SERV_PORT 6969
 #define INET_IP "192.168.1.20"
 
+
 using namespace std;
 
 // ================================================================
@@ -43,7 +44,6 @@ socklen_t clientAddrLength;
 extern struct sockaddr_in serverAddr;
 extern struct sockaddr_in clientAddr;
 
-
 // ==================================================================
 // ===================  结构体  ======================================
 // ==================================================================
@@ -55,8 +55,8 @@ void initializeServerAddr(struct sockaddr_in& addr, socklen_t &serverAddrLength)
     // sin -> socket internet
     addr.sin_family = AF_INET; // IPV4
     addr.sin_port = htons(SERV_PORT); // 定义端口号8888，已转换主机字节序为网络字节序
-    addr.sin_addr.s_addr = inet_addr(INET_IP); //设置本机IP，已把地址转换为整数
-    // addr.sin_addr.s_addr = htonl(INADDR_ANY); // 服务器将绑定到所有可用的网络接口上，而不是一个特定的IP地址
+    // addr.sin_addr.s_addr = inet_addr(INET_IP); //设置本机IP，已把地址转换为整数
+    addr.sin_addr.s_addr = htonl(INADDR_ANY); // 服务器将绑定到所有可用的网络接口上，而不是一个特定的IP地址
     serverAddrLength = sizeof(addr);
 }
 
@@ -83,28 +83,28 @@ public:
     static void clear(){system("clear");}
 };
 
+void printClientIP(sockaddr_in addr){
+    // 客户端PORT是不是不太对!!
+    cout<<"Client IP is "<<inet_ntoa(clientAddr.sin_addr)<<" / "<<SERV_PORT<<endl<<endl;; // 不能输出自动识别的ip
+}
+
+
+void printServerIP(sockaddr_in addr){
+    cout <<"Server IP is "<<inet_ntoa(serverAddr.sin_addr)<<" / "<<SERV_PORT<<endl<<endl;;
+}
+
+// 自带一个\n
+void printMessage(const char *msg){
+    cout << msg << "\n";
+}
+
+
 int errorIfValueLessThan_0(const int value ,const char *msg){
     if(value < 0){
         perror(msg);
         cerr << "Error code: " << errno << endl;
         return -1;
     }else return value;
-}
-
-int createSocket(){
-    // 创建套接字
-    // socket 函数返回值应该是3，因为0、1、2都被占用了
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0); //保存 socket 返回值 作为 监听套接字
-    if(errorIfValueLessThan_0(sockfd, "Failed to create socket") != -1) 
-    cout<<"- socket created! -\nSocket File Descriptor: "<<sockfd<<"\n\n";
-    return sockfd;
-}
-
-void closeSocket(int sockfd){
-    int i = close(sockfd);
-    if(errorIfValueLessThan_0(i, "Failed to close socket") != -1)
-    cout << "- socket ("<< sockfd <<") closed! -\n";
-    
 }
 
 int quitIfTypeQuit(){
@@ -120,15 +120,88 @@ int isQuit(char buffer[]){
 }
 
 
-// void printClientIP(in_addr addr){
-//     // 客户端IP和PORT是不是不太对
-//     "Client IP is "<<inet_ntoa(addr)<<" / "<<SERV_PORT<<endl<<endl;; 
-// }
+// ================================================================
+// =================  主要函数  ======================================
+// ================================================================
 
 
-// void printServerIP(){
-//     "Server IP is "<<inet_ntoa(clientAddr.sin_addr)<<" / "<<SERV_PORT<<endl<<endl;;
-// }
+
+//1. 创建套接字
+int createSocket(){
+    // socket 函数返回值应该是3，因为0、1、2都被占用了
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0); //保存 socket 返回值 作为 监听套接字
+    if(errorIfValueLessThan_0(sockfd, "Failed to create socket") != -1) 
+    cout<<"- socket created! -\nSocket File Descriptor: "<<sockfd<<"\n\n";
+    return sockfd;
+}
+
+//2. 绑定IP地址和端口号
+void bindSocketToServer(int sockfd){
+    int bindReturn = bind(sockfd, (struct sockaddr *)&serverAddr, static_cast<socklen_t>(serverAddrLength));
+    if(errorIfValueLessThan_0(bindReturn, "Failed to bind") != -1){
+        cout<<"- Bind successful -\n";
+        printServerIP(serverAddr);
+    }
+}
+
+//3. 设置监听套接字
+void startListeningOnSocket(int sockfd){
+    int listenReturn = listen(sockfd, 5);
+    if(errorIfValueLessThan_0(listenReturn, "Failed to listen") != -1)
+    cout<<"- Listen successful -\n";
+}
+
+//4.5 accept
+void acceptConnect(int &connectfd, int sockfd){
+    connectfd = accept(sockfd, (struct sockaddr *)&clientAddr, &clientAddrLength);
+    if(errorIfValueLessThan_0(connectfd, "Failed to connect and create a new connectfd") != -1)
+    {cout<<"- Client connected -\n";
+    printClientIP(clientAddr);}
+}
+
+//5. 数据接受和发送 read
+int readClientMessage(int connectfd){
+        char buffer[64];
+        while(1){
+            memset(buffer, 0, sizeof(buffer));
+            int readReturn = read(connectfd, buffer, 64);
+
+            if(readReturn < 0){
+                perror("Failed to read");
+                cerr << "Error code: " << errno << endl;
+                return -1;
+            }else if(readReturn == 0){
+                printMessage("\n-- The client disconnected! --");
+                printClientIP(clientAddr);
+                break;
+            }
+            
+            cout << "- read successful -\n"<<"recieved message ("<< readReturn <<" bytes):\n"<<buffer<<"\n\n";
+            return 0;
+
+            // if(errorIfValueLessThan_0(readReturn, "Failed to read buffer") != -1)
+            // cout << "- read successful -\n"<<"recieved message ("<< readReturn <<" bytes):\n"<<buffer<<"\n\n";
+            // else if(readReturn == 0){
+            //     cout << "\n-- The client disconnected! --\n";
+            //     cout<<"Client IP is "<<inet_ntoa(clientAddr.sin_addr)<<" / "<<SERV_PORT<<endl<<endl; // 不能输出自动识别的ip
+            //     break;
+            // }
+        }
+
+
+}
+
+
+
+void closeSocket(int sockfd){
+    int i = close(sockfd);
+    if(errorIfValueLessThan_0(i, "Failed to close socket") != -1)
+    cout << "- socket ("<< sockfd <<") closed! -\n";
+    
+}
+
+
+
 
 // void printMessageAndServerIP(const char *msg){
 //     cout << "- connect successful -\n" <<"server IP is "<<inet_ntoa(serverAddr.sin_addr)<<" / "<<SERV_PORT<<endl<<endl;; // 不能输出自动识别的ip;
