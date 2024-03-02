55f9a737b5767a449a13785d38cf9add409cf41d
Raven <1772133019@qq.com>
2024-03-02 22:15:43 +0800

    init

---
+++ CTPtest/main.cpp
@@ -0,0 +1,13 @@
+#include <ThostFtdcMdApi.h>
+#include <ThostFtdcTraderApi.h>
+#include <ThostFtdcUserApiDataType.h>
+#include <ThostFtdcUserApiStruct.h>
+
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <unordered_map>
+
+int main(){
+    std::cout<<"start\n";
+}
---
+++ CTPtest/readme.md
@@ -0,0 +1,4 @@
+# CTP 测试任务
+[HERE IS NOTION PAGE](https://doria42.notion.site/CTP-1-f52960064a534133b47d0a4120f028cc?pvs=4)
+
+思维导图在文件夹中,文件名是 CTP.xmind
